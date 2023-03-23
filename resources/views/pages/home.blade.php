9b7e241739106f58fdd2e893e5de9618a6aa59fb
Oreo-new <clintscopy@gmail.com>
2023-03-23 01:56:25 -0400

    fixing homapge

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,19 +1,26 @@
 <x-app-layout>
  
     <div class="fixed w-full top-0 z-10 bg-[#FFFFFF] py-2">
-        <img src="{{ asset('images/langen-logo.jpeg') }}" alt="langen" class="w-80 mx-auto">
-    </div>
-    <div id="" class="flex  items-center w-full h-screen bg-no-repeat bg-cover home-cover" style="background-image: url({{ asset('/storage/' .$page->image) }});">
+        <div class="flex items-center content-between w-full">
+            <img src="{{ asset('images/langen-logo.jpeg') }}" alt="langen" class="w-80 mx-auto">
+            <ul class="list-none flex m-0 p-0 w-full items-center justify-end ">
+                <li class="px-6 text-xl hover:text-blue-700"><a href="#">Accomodation</a></li>
+                <li class="px-6 text-xl hover:text-blue-700"><a href="#">Activities</a></li>
+                <li class="px-6 text-xl hover:text-blue-700"><a href="#">Transportation</a></li>
+            </ul>
+        </div>
         
-        <div class="w-full block">
-            <div class="text-3xl md:text-5xl text-center text-[#1064BA] merri">Greetings to the USA</div>
-            <div class="text-xl md:text-4xl text-center text-[#FE9601] roboto mt-10">Welcome to Lagen Island! <br />
+    </div>
+    <div id="" class="flex  items-center w-full h-screen bg-no-repeat bg-cover home-cover bg-center" style="background-image: url({{ asset('/storage/' .$page->image) }});">
+        
+        <div class="w-full block mt-60">
+            <div class="text-2xl md:text-4xl text-center text-white roboto mt-10">Welcome to Lagen Island! <br />
                 The island paradise</div>
 
 
             <div x-data="{ open: false }">
-                <button class="mx-auto block border-2 border-[#FE9601] text-white mt-8 px-4 py-2 roboto text-sm md:text-lg cursor-pointer z-30 pointer-events-auto" @click="open = ! open">
-                    Our US Booking partners
+                <button class="mx-auto block bg-blue-500 hover:bg-blue-700 rounded text-white mt-8 px-4 py-2 roboto text-sm md:text-lg cursor-pointer z-30 pointer-events-auto" @click="open = ! open">
+                    Our US Booking Partners
                 </button>
 
                 <div x-show="open" @click.outside="open = false"
@@ -25,7 +32,7 @@
                     x-transition:leave-end="opacity-0 right-10">
                     <div class="z-10" role="dialog" aria-modal="true">
                         <div class="fixed inset-0 flex items-center bg-gray-500 bg-opacity-75 transition-opacity z-40 top-0 bottom-0 w-full h-full">
-                            <div class="w-full md:w-[400px] md:h-[400px] bg-white relative mx-2 md:mx-auto rounded p-2 md:p-10">
+                            <div class="w-full md:w-[350px] md:h-[350px] bg-white relative mx-2 md:mx-auto rounded p-2 md:p-10">
                                 <span class="absolute top-2 right-2 cursor-pointer" @click="open = false">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
